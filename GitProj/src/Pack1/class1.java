04cd5e4cb0e4112330d63e1c7c11c7ad0aa38d7c
paddu <padmagrla@gmail.com>
2017-09-25 18:15:45 +0200

    TC

---
+++ GitProj/src/Pack1/class1.java
@@ -0,0 +1,18 @@
+package Pack1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class class1 {
+	public static void main(String[] args){
+	WebDriver driver=new FirefoxDriver();
+	 
+     driver.get("http://www.ksrtc.in/oprs-web/");
+
+     driver.manage().window().maximize();
+
+     driver.findElement(By.xpath(".//*[@id='searchBtn']")).click();
+	}
+
+}
